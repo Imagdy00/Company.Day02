dfb579533f6f35ceb0df1fe53b8d8f98f3725fa0
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-03-13 16:03:21 +0200

    Create Department Controller And Allow DI for all Services

--- Company.Day02.DAL/Data/Contexts/CompanyDbContext.cs
+++ Company.Day02.DAL/Data/Contexts/CompanyDbContext.cs
@@ -11,14 +11,14 @@ namespace Company.Day02.DAL.Data.Contexts
 {
     public class CompanyDbContext : DbContext
     {
-        public CompanyDbContext() : base()
+        public CompanyDbContext(DbContextOptions <CompanyDbContext> options) : base(options)
         {
 
         }
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            optionsBuilder.UseSqlServer("Server = . ; Database = CompanyG02 ;  Trusted_Connection = True ; TrustServerCertificate = True ;  ");
-        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer("Server = . ; Database = CompanyG02 ;  Trusted_Connection = True ; TrustServerCertificate = True ;  ");
+        //}
 
         public DbSet<Department> Departments { get; set; }
 
---
+++ Company.Day02.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,24 @@
+﻿using Company.Day02.BLL.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace Company.Day02.PL.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentrepository;
+
+        public DepartmentController(IDepartmentRepository departmentRepository)
+        {
+            _departmentrepository = departmentRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+          
+            var departments = _departmentrepository.GetAll();
+            return View(departments);
+        }
+    }
+}
