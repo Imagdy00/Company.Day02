91b09f07cfb52e6d697c912429d65c14084e8866
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-03-28 16:23:54 +0200

    implementation of DAL for Employee Service

---
+++ Company.Day02.DAL/Data/Configrations/EmployeeConfigrations.cs
@@ -0,0 +1,19 @@
+﻿using Company.Day02.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.Day02.DAL.Data.Configrations
+{
+    public class EmployeeConfigrations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+            builder.Property(E => E.Salary).HasColumnType("decimal(18,2)");
+        }
+    }
+}
