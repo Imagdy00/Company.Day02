620b692da279ec2eaa411b12f1cb6a1e0d7f461d
Imagdy00 <125517448+Imagdy00@users.noreply.github.com>
2025-03-29 17:40:31 +0200

    implementation of PL for employee service

---
+++ Company.Day02.PL/Controllers/EmployeeController.cs
@@ -0,0 +1,162 @@
+﻿using Company.Day02.BLL.Interfaces;
+using Company.Day02.DAL.Models;
+using Company.Day02.PL.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.Day02.PL.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+
+        public EmployeeController (IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+
+            var employees = _employeeRepository.GetAll();
+            return View(employees);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+
+        }
+
+
+        [HttpPost]
+        public IActionResult Create(CreateEmployeeDto model)
+        {
+            if (ModelState.IsValid)
+            {
+                var employee = new Employee()
+                {
+                    Name = model.Name,
+                    Address = model.Address,
+                    Age = model.Age,
+                    CreateAt = model.CreateAt,
+                    HiringDate = model.HiringDate,
+                    Email = model.Email,
+                    IsActive = model.IsActive,
+                    IsDeleted = model.IsDeleted,
+                    Phone = model.Phone,
+                    Salary = model.Salary,
+                };
+                var count = _employeeRepository.Add(employee);
+
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+
+        }
+
+
+        [HttpGet]
+        public IActionResult Details(int? id, string viewName = "Details")
+        {
+            if (id is null) return BadRequest("Invalid Id");
+            var employee = _employeeRepository.Get(id.Value);
+            if (employee is null) return NotFound(new { StatusCode = 404, message = $"employee: with id {id} is not found" });
+            return View(viewName, employee);
+        }
+
+
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            //if (id is null) return BadRequest("Invalid Id");
+            //var department = _departmentrepository.Get(id.Value);
+            //if (department is null) return NotFound(new { StatusCode = 404, message = $"department: with id {id} is not found" });
+
+            return Details(id, "Edit");
+        }
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit([FromRoute] int id, Employee model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                if (id != model.Id) return BadRequest();
+                var count = _employeeRepository.Update(model);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+
+
+
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public IActionResult Edit([FromRoute] int id, UpdateDepartmentDto model)
+        //{
+
+
+        //    if (ModelState.IsValid)
+        //    {
+        //        var department = new Department
+        //        {
+        //            Id = id,
+        //            Name = model.Name,
+        //            Code = model.Name,
+        //            CreateAt = model.CreateAt
+        //        };
+        //        var count = _departmentrepository.Update(department);
+        //        if (count > 0)
+        //        {
+        //            return RedirectToAction(nameof(Index));
+        //        }
+        //    }
+        //    return View(model);
+        //}
+
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            //if (id is null) return BadRequest("Invalid Id");
+            //var department = _departmentrepository.Get(id.Value);
+            //if (department is null) return NotFound(new { StatusCode = 404, message = $"department: with id {id} is not found" });
+            return Details(id, "Delete");
+        }
+
+
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete([FromRoute] int id, Employee model)
+        {
+
+
+            if (ModelState.IsValid)
+            {
+                if (id != model.Id) return BadRequest();
+                var count = _employeeRepository.Delete(model);
+                if (count > 0)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Company.Day02.PL/Dtos/CreateEmployeeDto.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Company.Day02.PL.Dtos
+{
+    public class CreateEmployeeDto
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Range(22,60 , ErrorMessage = "Age must be between 22 and 60")]
+        public int? Age { get; set; }
+        [DataType(DataType.EmailAddress , ErrorMessage = "Email is not valid")]
+        public string Email { get; set; }
+
+        [RegularExpression(@"[0-9]{1,3}-[a-zA-Z]{5,10}-[a-zA-Z]{4,10}-[a-zA-Z]{5,10}$" , 
+            ErrorMessage = "Address must  be like 123-street-city-country")]
+        public string Address { get; set; }
+
+        [Phone]
+        public string Phone { get; set; }
+        [DataType(DataType.Currency)]
+        public decimal Salary { get; set; }
+
+
+        public bool IsActive { get; set; }
+        public bool IsDeleted { get; set; }
+        [DisplayName("Hiring Date")]
+        public DateTime HiringDate { get; set; }
+        [DisplayName("Date Of Creation")]
+        public DateTime CreateAt { get; set; }
+    }
+}
